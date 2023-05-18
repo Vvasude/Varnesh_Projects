73660ef7ebc0da5bb5e93e3c2847efe9f77f8edb
Vvasude <86793451+Vvasude@users.noreply.github.com>
2023-05-18 17:06:30 -0400

    created homepage and create page

---
+++ reactWebDevelopment/client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import image from './assets/artificial-intelligence.png'
+
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={image} alt="logo" className="w-20 object-contain" />
+      </Link>
+
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Generate</Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
