a30f59ae88c93e2b7cb4dc214cc479fc9fe9f683
Vvasude <86793451+Vvasude@users.noreply.github.com>
2023-05-19 13:38:10 -0400

    added titles and bio to the website

---
+++ reactWebDevelopment/client/src/components/index.js
@@ -0,0 +1,10 @@
+import Card from './Card';
+import FormField from './FormField';
+import Loader from './Loader';
+
+//export everything as one object
+export {
+  Card,
+  FormField,
+  Loader,
+};
