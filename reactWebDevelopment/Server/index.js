946982858db1eee809e85c1812dbd6ec4fb22df8
Vvasude <86793451+Vvasude@users.noreply.github.com>
2023-05-21 12:20:07 -0400

    created routes to connect to API

--- reactWebDevelopment/Server/index.js
+++ reactWebDevelopment/Server/index.js
@@ -3,16 +3,24 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongoConnection/connection.js';
-import postRoutes from './routes/postRoutes.js';
+// import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) ;
+// app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post', postRoutes);
+// app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
