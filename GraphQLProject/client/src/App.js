9adffd00c6e4657836eca1f76f4989dc8ec4bc46
Vvasude <varnesh.vasudevan@gmail.com>
2024-11-05 17:11:52 -0500

    bug fixes

--- GraphQLProject/client/src/App.js
+++ GraphQLProject/client/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Project from './pages/Project';
-import {ApolloProvider, ApolloClient, InMemoryCache, from} from "@apollo/client";
+import {ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
 
 
 const cache = new InMemoryCache({
